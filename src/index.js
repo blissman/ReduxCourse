36656baa770a1f2584fae492a42a86eefdea306a
Bliss Man <bliss.man@cbc.ca>
2018-09-28 15:43:50 -0400

    render Hi to page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // create component to generate HTML
+const App = () => {
+	return <div>Hi!</div>;
+}
+
 // put generated component HTML into the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
